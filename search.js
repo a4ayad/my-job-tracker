ff5c65da30979eac497084adb259702928dcf5ae
Ahmed Ayad <aahmed@msimbo.com>
2021-01-24 17:44:50 -0500

    add updatePage and getJob functions

--- search.js
+++ search.js
@@ -8,9 +8,12 @@ const SEARCH_COUNTRY = 'us'
 // because we are call the dynamic "searchJobs"
 searchForm.addEventListener('submit', async function (e) {
   e.preventDefault()
+  getJob ()
+})
 
+// Define a function 
+async function getJob() {
   const jobName = jobType.value
-
   // we get the results from the API
   let jobResults = await searchJobs(jobName)
 
@@ -21,22 +24,31 @@ searchForm.addEventListener('submit', async function (e) {
   // there are things on the object too
   let jobsFound = jobResults.count
 
-  resultSection.innerHTML = `
-<div id="result-section" class="p-16">
-<h1>${jobsFound} jobs found for <strong>${jobName}</strong> in ${SEARCH_COUNTRY.toUpperCase()}</h1>
-</div>`
-  jobs.forEach(function (job) {
-    const div = document.createElement('div')
-    div.innerHTML = `<h4><a href="${job.redirect_url}">${job.title}</a> | ${job.location.display_name}</h4> <p>${job.description}</p>`
-    resultSection.appendChild(div)
-  })
-})
+  updatePage(jobs, jobsFound, jobName)
+}
 
-async function searchJobs (jobString, jobsCount = 3, country = SEARCH_COUNTRY) {
+// Define a function to update the page with results
+function updatePage(jobs, jobsFound, jobName) {
+  resultSection.innerHTML = `
+  <div id="result-section" class="p-16">
+  <h1>${jobsFound} jobs found for 
+  <strong>${jobName}</strong> in ${SEARCH_COUNTRY.toUpperCase()}</h1>
+  </div>`
+    jobs.forEach(function (job) {
+      const div = document.createElement('div')
+      div.innerHTML = `<h4><a href="${job.redirect_url}">${job.title}</a> | 
+      ${job.location.display_name}</h4> <p>${job.description}</p>`
+
+      resultSection.appendChild(div)
+    })
+}
+
+async function searchJobs (jobString, jobsCount = 10, country = SEARCH_COUNTRY) {
 
   // see https://gist.github.com/imdeletosh/f13c339b5f8e0a62ebe973a4ac86c3c0 for a breakdown of this
   const url = `http://api.adzuna.com/v1/api/jobs/${country}/search/1?app_id=a7547f34&app_key=92b62ee9bfd90c11c097004b51438beb&results_per_page=${jobsCount}&what=${jobString}&content-type=application/json`
-  console.log(url)
+  
+  // console.log(url)
 
 //   https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=a7547f34&app_key=92b62ee9bfd90c11c097004b51438beb&results_per_page=20&what=javascript%20developer&content-type=application/json
   
