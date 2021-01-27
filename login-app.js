d4a1931f7f2c797b38efdb907a7056e6468af36f
Ahmed Ayad <aahmed@msimbo.com>
2021-01-27 08:48:16 -0500

    add placeholder for firebase

--- login-app.js
+++ login-app.js
@@ -25,7 +25,7 @@ form.addEventListener('submit', function () {
 
 //   `<a href="./search-display.html"></a>`
 
-  validateEmpty(username)
+  // validateEmpty(username)
   validateEmpty(email)
   validateEmpty(password1)
   validateEmpty(password2)
@@ -95,3 +95,73 @@ function validateMinLength (input) {
     showSuccess(input)
   }
 }
+
+  // Our web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDaQ1_fMKNBRJIGBXOMAWCL_f2gaVLbQV4",
+    authDomain: "my-job-tracker.firebaseapp.com",
+    projectId: "my-job-tracker",
+    storageBucket: "my-job-tracker.appspot.com",
+    messagingSenderId: "1072074186001",
+    appId: "1:1072074186001:web:33462079bc0dbb07afc0b0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+   // add a reference to the authentication app 
+
+    const auth = firebase.auth();
+
+    // Define register function
+    function register() {
+      var email = document.getElementById("email")
+      var password = document.getElementById("password")
+
+      // Create a new user account associated with the specified email 
+      // address and password in the authentication app by creating a promise
+      const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+
+      // Setup an alert system that may catch error that may occur: improper email 
+      // formating or if the password is less than 6 characters long
+      promise.catch(e => alert(e.message));
+      alert("Registered!");
+    }
+
+    // Define signIn/login function for registered users
+    function signIn() {
+      var email = document.getElementById("email")
+      var password = document.getElementById("password")
+      const promise = auth.signInUserWithEmailAndPassword(email.value, password.value);
+      promise.catch(e => alert(e.message));
+      alert("Signed In!" + email);
+    }
+
+    // Define signOut functiom
+    function signOut() {
+      auth.signOut();
+      alert("Sihned Out!");
+    }
+
+    auth.onAuthStaticChanged(function(user){
+
+      if(user) {
+        var email = user.email;
+        alert("Active User " + email);
+        // is signed in
+      } else {
+        alert("No Active User!")
+        // no user is sgned in
+      }
+    })
+
+    // Testing example code from https://firebase.google.com/learn/codelabs/firebase-get-to-know-web#4
+
+    // Firebase App (the core Firebase SDK) is always required
+// and must be listed first
+import * as firebase from "firebase/app";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+import * as firebaseui from "firebaseui";
